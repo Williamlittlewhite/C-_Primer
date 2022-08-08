b5c5888d8006bd017e8a5430e0e3ca2be2625bf9
William <bzx517291467@gmail.com>
2022-08-08 19:58:36 +0800

    2022.8.8

---
+++ C++Primier/src/1.1.cpp
@@ -0,0 +1,17 @@
+//Á·Ï°1.1
+//g++ 1.1.cpp -o 1.1   
+//1.1   
+//echo %ERRORLEVEL
+//int main()
+//{
+//	return 0;
+//}
+
+//Á·Ï°1.2  
+//g++ 1.2.cpp -o 1.2   
+//1.2   
+//echo %ERRORLEVEL
+//int main()
+//{
+//	return -1;
+//}
