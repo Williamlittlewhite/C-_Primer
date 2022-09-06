0d9273d5e54872dfc07102c154f1baa4e87088c9
William <bzx517291467@gmail.com>
2022-09-06 22:17:04 +0800

    2022.9.6

---
+++ C++Primier/src/9.3.2.cpp
@@ -0,0 +1,11 @@
+//练习9.23&&9.24 at会抛出异常而其他取值方法会直接退出
+//#include<vector>
+//#include<iostream>
+//int main()
+//{
+//	std::vector<int> v{ 5 };
+//
+//	std::cout << v.at(0) << " " << v[0] << " " << v.front() << " " << *v.begin();
+//	std::vector<int> v1;
+//	std::cout << v1.at(0) << " " << v1[0] << " " << v1.front() << " " << *v1.begin();
+//}
