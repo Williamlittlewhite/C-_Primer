6197df05fad6b5d5c856c15c61a8d35dde97c3e4
William <bzx517291467@gmail.com>
2022-10-25 23:35:44 +0800

    2022.10.25

---
+++ cppPrimerSolution/src/第三部分 类设计者的工具/第十六章 模板与泛型编程/16.2.7.cpp
@@ -0,0 +1,28 @@
+#include<utility>
+#include<iostream>
+template<typename F,typename T1, typename T2>
+void flip(F f, T1&& t1, T2&& t2)
+{
+	f(std::forward<T2>(t2), std::forward<T1>(t1));
+}
+void f(int v1, int& v2)
+{
+	std::cout << v1 << " " << ++v2 << std::endl;
+}
+
+void g(int&& i, int& j)
+{
+	std::cout << i << " " << j << std::endl;
+}
+template<typename F, typename T1, typename T2>
+void flip1(F f, T1 t1, T2 t2)
+{
+	f(t2, t1);
+}
+
+template<typename F, typename T1, typename T2>
+void flip2(F f, T1&& t1, T2&& t2)
+{
+	f(t2, t1);
+}
+
---
+++ cppPrimerSolution/src/第三部分 类设计者的工具/第十六章 模板与泛型编程/16.3.cpp
@@ -0,0 +1,61 @@
+//练习16.48：
+#include<string>
+#include<iostream>
+//template<typename T> std::string debug_rep(const T& t)
+//{
+//	std::cout << t;
+//	return std::string(t);
+//}
+//template<typename T>std::string debug_rep(T* t)
+//{
+//	std::cout << "pointer: " << t;
+//	if (t)
+//		std::cout << " " << debug_rep(*t);
+//	else
+//		std::cout << " null pointer";
+//	return std::string(t);
+//}
+//std::string debug_rep(const std::string& s) {
+//	return '"' + s + '"';
+//}
+//std::string debug_rep(char* p)
+//{
+//	return debug_rep(std::string(p));
+//}
+//std::string debug_rep(const char* p)
+//{
+//	return debug_rep(std::string(p));
+//}
+//
+//int main()
+//{
+//	const char* p = "Hello world";
+//	std::cout << debug_rep(p);
+//	return 0;
+//}
+
+//练习16.50:
+//template<typename T>void f(T)
+//{
+//	std::cout << "f(T),T是" <<typeid(T).name() << std::endl;
+//}
+//template<typename T>void f(const T*)
+//{
+//	std::cout << "f(const T*),T是" << typeid(T).name() << std::endl;
+//}
+//template<typename T>void g(T)
+//{
+//	std::cout << "g(T),T是" << typeid(T).name() << std::endl;
+//}
+//template<typename T>void g(T*)
+//{
+//	std::cout << "g(T*),T是" << typeid(T).name() << std::endl;
+//}
+//int main()
+//{
+//	int i = 42, * p = &i;
+//	const int ci = 0, * p2 = &ci;
+//	g(42); g(p); g(ci); g(p2);
+//	f(42); f(p); f(ci); f(p2);
+//	return 0;
+//}
